3dc74163355379cf381bed81cffa5f6ad282c34c
ecmascriptguru <ecmascript.guru@gmail.com>
2017-04-08 09:30:42 -0400

    static version created

--- src/App.js
+++ src/App.js
@@ -5,17 +5,72 @@ import './App.css';
 class SearchBar extends React.Component {
     render() {
         return (
-            <div>
-            </div>
+            <form>
+                <input type="text" placeholder="Search..." />
+                <p>
+                    <input type="checkbox" />
+                    {" "}
+                    Only Show Products in stock.
+                </p>
+            </form>
+        );
+    }
+}
+
+class ProductCategoryRow extends React.Component {
+    render() {
+        return (
+            <tr>
+                <th colSpan="2">{this.props.category}</th>
+            </tr>
+        );
+    }
+}
+
+class ProductRow extends React.Component {
+    render() {
+        let name = (this.props.product.stocked) ?
+            this.props.product.name :
+            <span style={{color: "red"}}>
+                {this.props.product.name}
+            </span>;
+
+        return (
+            <tr>
+                <td>{name}</td>
+                <td>{this.props.product.price}</td>
+            </tr>
         );
     }
 }
 
 class ProductTable extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    // }
+
     render() {
+        let rows = [];
+        let lastCategory = null;
+        this.props.products.forEach(function(product) {
+            if (product.category !== lastCategory) {
+                rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
+            }
+            rows.push(<ProductRow product={product} key={product.name} />);
+            lastCategory = product.category;
+        });
         return (
-            <div>
-            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {rows}
+                </tbody>
+            </table>
         );
     }
 }
